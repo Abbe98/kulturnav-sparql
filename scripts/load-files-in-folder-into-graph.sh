b774b4e51a8f064ef0e5b1521fddf8a4844608d8
Abbe98 <albin.post@gmail.com>
2025-03-27 13:00:12 +0100

    fuseki: update loading script for kulturnav

--- scripts/load-files-in-folder-into-graph.sh
+++ scripts/load-files-in-folder-into-graph.sh
@@ -1,7 +1,7 @@
-for file in *.jsonld; do 
+for file in *.rdf; do 
     if [ -f "$file" ]; then 
         echo "Loading $file into graph"
         # load json-ld file into graph
-        curl --include -X POST -H "Content-Type: application/ld+json" -d @$file http://127.0.0.1:3030/fornpunkt?default ;
+        curl --include -X POST -H "Content-Type: application/rdf+xml" -d @$file http://127.0.0.1:3030/kulturnav?default ;
     fi
 done
