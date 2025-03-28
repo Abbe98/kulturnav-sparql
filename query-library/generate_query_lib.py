7cb7334bfcb5a0aa0b8453a7afe2363593f2172f
Abbe98 <albin.post@gmail.com>
2025-03-28 12:33:34 +0100

    queries: more kulturnav queries

--- query-library/generate_query_lib.py
+++ query-library/generate_query_lib.py
@@ -1,19 +1,37 @@
 import os
 import json
 
+"""
+#defaultView:PieChart
+#title: Könsfördelning som cirkeldiagram
+
+
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+
+SELECT ?label (COUNT(?person) AS ?count) WHERE {
+  ?person a <http://schema.org/Person> .
+  ?person skos:prefLabel ?name .
+  ?person foaf:gender ?label .
+}
+GROUP BY ?label
+"""
+
+
 queries = list()
 for filename in os.listdir('query-library/src'):
     with open('query-library/src/' + filename, 'r') as f:
-        title, tags, body = f.read().split('\n', 2)
-        title = title.replace('#title: ', '')
-        tags = tags.replace('#tags: ', '').split(',')
-        body = body.replace('\n', '', 2)
+        content = f.read()
+        title = ''
+        for line in content.split('\n'):
+            if line.startswith('#title'):
+                title = line
+                title = title.split(':')[1].strip()
+                break
 
         query = {}
         query['title'] = title
-        query['tags'] = tags
-        query['body'] = body
-        
+        query['body'] = content
         queries.append(query)
 
 with open('thor-configuration/queries.json', 'w') as outfile:
